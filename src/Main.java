c16a4f1279ebfcff815910f4b65492bf352f3542
sakura636 <yingrui_wang@my.ofs.edu.sg>
2025-10-03 10:36:16 +0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        Person object= new Person();
+        object.setName("sakura");
+        System.out.println(object.getName());
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String inputName)
+    {
+        this.name=inputName;
+    }
+}
